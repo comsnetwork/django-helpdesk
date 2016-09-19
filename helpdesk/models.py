bce3226afcd23f37b098f7c55f3837c8cbf638f4
=yeago <yeago999@gmail.com>
2016-09-19 14:26:34 -0700

    str to int

--- helpdesk/models.py
+++ helpdesk/models.py
@@ -1142,7 +1142,7 @@ class CustomField(models.Model):
 
     label = models.CharField(
         _('Label'),
-        max_length='30',
+        max_length=30,
         help_text=_('The display label for this field'),
         )
 
